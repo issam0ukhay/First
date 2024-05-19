ae6b339543ebf1201b87f3e8c2ee9681133e5e28
Issam Oukhay <issam.oukhay@ensi-uma.tn>
2024-05-19 19:15:16 +0100

    Last

--- src/app/dialog-template-demo.ts
+++ src/app/dialog-template-demo.ts
@@ -4,10 +4,20 @@ import { ImportsModule } from './imports';
 @Component({
     selector: 'dialog-template-demo',
     templateUrl: './dialog-template-demo.html',
+    styleUrl:'./dialog.component.scss',
     standalone: true,
     imports: [ImportsModule]
 })
 export class DialogTemplateDemo {
+
+    members = [
+        { name: 'Entity1', image: 'amyelsner.png' },
+        { name: 'Entity2', image: 'bernardodominic.png' },
+        { name: 'Entity3', image: 'ionibowcher.png' },
+        { name: 'Entity4', image: 'bernardodominic.png' },
+        { name: 'Entity5', image: 'ionibowcher.png' }
+    ];
+
     visible: boolean = false;
 
     showDialog() {
